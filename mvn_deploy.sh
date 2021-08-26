31c98376aed8f91053516cd89a4fa3a1f8e4ef11
August <august.zhou@guanaitong.com>
2021-08-26 21:45:59 +0800

    deploy scrpit

---
+++ mvn_deploy.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+set -ex
+
+function deploy_parent() {
+   rm -rf tmp && mkdir tmp
+   cp grus-boot-starter-parent_pom.xml tmp/.flattened-pom.xml
+   GRUS_VERSION=$(mvn org.apache.maven.plugins:maven-help-plugin:3.2.0:evaluate -Dexpression=revision -q -DforceStdout)
+   SPRING_BOOT_VERSION=$(mvn org.apache.maven.plugins:maven-help-plugin:3.2.0:evaluate -Dexpression=spring-boot.version -q -DforceStdout)
+   sed -i "s/GRUS_VERSION/$GRUS_VERSION/g" tmp/.flattened-pom.xml
+   sed -i "s/SPRING_BOOT_VERSION/$SPRING_BOOT_VERSION/g" tmp/.flattened-pom.xml
+   mvn install -f tmp/.flattened-pom.xml -Dmaven.test.skip=true
+}
+
+if [[ $GIT_BRANCH =~ "feature" ]]; then
+  echo "end"
+else
+   mvn deploy -Dmaven.test.skip=true
+   deploy_parent
+fi
