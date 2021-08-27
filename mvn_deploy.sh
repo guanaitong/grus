dba4fe311098b0a9fad7c812186e4da3ddf3ce50
August <august.zhou@guanaitong.com>
2021-08-27 17:48:57 +0800

    test env

--- mvn_deploy.sh
+++ mvn_deploy.sh
@@ -12,7 +12,7 @@ function deploy_parent() {
    mvn deploy -f tmp/.flattened-pom.xml -Dmaven.test.skip=true
 }
 
-printenv
+echo "branch is " $GIT_BRANCH
 # deploy for branches of master or release
 if [[ $GIT_BRANCH =~ "master" ]] || [[ $GIT_BRANCH =~ "release" ]] ; then
   mvn deploy -Dmaven.test.skip=true
