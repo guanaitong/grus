5ddde79c420e9d5ee89500dfdd3f39f6a7211d54
August <august.zhou@guanaitong.com>
2021-09-01 10:39:36 +0800

    add image & news msg type to group bot

--- grus-framework/grus-core/src/main/java/com/ciicgat/sdk/util/frigate/GroupBot.java
+++ grus-framework/grus-core/src/main/java/com/ciicgat/sdk/util/frigate/GroupBot.java
@@ -5,66 +5,115 @@
 
 package com.ciicgat.sdk.util.frigate;
 
-import com.ciicgat.sdk.lang.tool.Bytes;
+import com.ciicgat.grus.json.JSON;
+import com.ciicgat.sdk.util.http.HttpClientHelper;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.apache.commons.codec.digest.DigestUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-import java.time.Duration;
+import java.util.Base64;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 
 /**
  * Created by Albert on 2019-05-08.
  */
 public abstract class GroupBot {
+    private static final Logger LOGGER = LoggerFactory.getLogger(GroupBot.class);
 
     private static final String WEB_HOOK_PATTERN = "https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=%s";
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(GroupBot.class);
-
-    private static final HttpClient HTTP_CLIENT = HttpClient
-            .newBuilder()
-            .connectTimeout(Duration.ofMillis(5000))
-            .followRedirects(HttpClient.Redirect.NEVER)
-            .build();
 
     public static void sendTextMsg(String groupBotKey, String content) {
-        final String textMsg = new MessagePacket("text", new MessagePacket.Text(content)).toString();
-        FrigateClient.EXECUTOR_SERVICE.execute(() ->
-                executeRequest(String.format(WEB_HOOK_PATTERN, groupBotKey), textMsg)
-        );
+        sendTextMsg(groupBotKey, content, null, null);
+    }
+
+    public static void sendTextMsg(String groupBotKey, String content, List<String> mentionedList, List<String> mentionedMobileList) {
+        Map<String, Object> textMap = new HashMap<>(2);
+        textMap.put("content", content);
+        if (Objects.nonNull(mentionedList) && !mentionedList.isEmpty()) {
+            textMap.put("mentioned_list", mentionedList);
+        }
+        if (Objects.nonNull(mentionedMobileList) && !mentionedMobileList.isEmpty()) {
+            textMap.put("mentioned_mobile_list", mentionedMobileList);
+        }
+        Map<String, Object> msg = Map.of("msgtype", "text", "text", textMap);
+        executeRequest(groupBotKey, msg);
     }
 
     public static void sendMarkdownMsg(String groupBotKey, String content) {
-        String markdownMsg = new MessagePacket("markdown", new MessagePacket.Markdown(content)).toString();
-        FrigateClient.EXECUTOR_SERVICE.execute(() ->
-                executeRequest(String.format(WEB_HOOK_PATTERN, groupBotKey), markdownMsg)
-        );
+        Map<String, Object> msg = Map.of("msgtype", "markdown", "markdown", Map.of("content", content));
+        executeRequest(groupBotKey, msg);
     }
 
-    private static void executeRequest(String url, String content) {
+    public static void sendImageMsg(String groupBotKey, byte[] imageData) {
+        Map<String, Object> msg = Map.of("msgtype", "image",
+                "image", Map.of("base64", Base64.getEncoder().encodeToString(imageData), "md5", DigestUtils.md5Hex(imageData)));
+        executeRequest(groupBotKey, msg);
+    }
 
-        try {
-            HttpRequest.BodyPublisher bodyPublisher = HttpRequest.BodyPublishers.ofByteArray(Bytes.toBytes(content));
-            URI uri = new URI(url);
-            final HttpRequest httpRequest = HttpRequest
-                    .newBuilder()
-                    .uri(uri)
-                    .timeout(Duration.ofMillis(30000))
-                    .header("Content-Type", "application/json")
-                    .POST(bodyPublisher)
-                    .build();
+    public static void sendNewsMsg(String groupBotKey, List<NewsArticle> newsArticles) {
+        Map<String, Object> msg = Map.of("msgtype", "news",
+                "news", Map.of("articles", newsArticles));
+        executeRequest(groupBotKey, msg);
+    }
 
-            HttpResponse<String> httpResponse = HTTP_CLIENT.send(httpRequest, HttpResponse.BodyHandlers.ofString());
-            if (httpResponse.statusCode() != 200) {
-                LOGGER.info("frigate notify failed , code {} ,body {}", httpResponse.statusCode(), httpResponse.body());
-            }
-        } catch (Exception ex) {
-            LOGGER.error("企业微信请求异常", ex);
+    public static final class NewsArticle {
+        private String title;
+        private String description;
+        private String url;
+        @JsonProperty("picurl")
+        private String picUrl;
+
+        public String getTitle() {
+            return title;
         }
 
+        public NewsArticle setTitle(String title) {
+            this.title = title;
+            return this;
+        }
+
+        public String getDescription() {
+            return description;
+        }
+
+        public NewsArticle setDescription(String description) {
+            this.description = description;
+            return this;
+        }
+
+        public String getUrl() {
+            return url;
+        }
+
+        public NewsArticle setUrl(String url) {
+            this.url = url;
+            return this;
+        }
+
+        public String getPicUrl() {
+            return picUrl;
+        }
+
+        public NewsArticle setPicUrl(String picUrl) {
+            this.picUrl = picUrl;
+            return this;
+        }
+    }
+
+
+    private static void executeRequest(String groupBotKey, Object body) {
+        FrigateClient.EXECUTOR_SERVICE.execute(() -> {
+            try {
+                HttpClientHelper.postJson(String.format(WEB_HOOK_PATTERN, groupBotKey), JSON.toJSONString(body));
+            } catch (Exception ex) {
+                LOGGER.error("企业微信请求异常", ex);
+            }
+        });
     }
 
 
--- grus-framework/grus-core/src/test/java/com/ciicgat/sdk/util/frigate/GroupBotTest.java
+++ grus-framework/grus-core/src/test/java/com/ciicgat/sdk/util/frigate/GroupBotTest.java
@@ -5,7 +5,14 @@
 
 package com.ciicgat.sdk.util.frigate;
 
+import com.ciicgat.sdk.lang.tool.ReadResourceUtils;
+import org.apache.commons.io.IOUtils;
+import org.junit.After;
+import org.junit.Test;
+
+import java.io.IOException;
 import java.util.Date;
+import java.util.List;
 
 /**
  * Created by Albert on 2019-05-08.
@@ -15,38 +22,45 @@ public class GroupBotTest {
     /**
      * 企业微信机器人的 key，用来发送报警通知
      */
-    private static final String GROUP_BOT_KEY = "your own group bot key";
+    private static final String GROUP_BOT_KEY = "ceed6aa3-ca81-4e9e-9638-9bc23e115a16";
 
-    //    @Test
-    public void sendMarkdownMsg() {
-
-        StringBuffer markdown = new StringBuffer();
-        markdown.append("# Grus 构建知会").append("\n");
-        markdown.append(">").append("警告:").append("<font color=\\\"warning\\\">这是警告（黄色）</font>").append("\n");
-        markdown.append(">").append("提示:").append("<font color=\\\"info\\\">这是提示（绿色）</font>").append("\n");
-        markdown.append(">").append("注释:").append("<font color=\\\"comment\\\">这是备注（灰色）</font>").append("\n");
-        markdown.append(">").append(new Date());
-
-        GroupBot.sendMarkdownMsg(GROUP_BOT_KEY, markdown.toString());
-
+    @After
+    public void tearDown() throws Exception {
         try {
-            Thread.sleep(500);
+            Thread.sleep(1000);
         } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
         }
 
     }
 
-    //    @Test
+    @Test
+    public void sendMarkdownMsg() {
+        StringBuffer markdown = new StringBuffer();
+        markdown.append("# Grus 构建知会").append("\n");
+        markdown.append(">").append("警告:").append("<font color=\"warning\">这是警告（黄色）</font>").append("\n");
+        markdown.append(">").append("提示:").append("<font color=\"info\">这是提示（绿色）</font>").append("\n");
+        markdown.append(">").append("注释:").append("<font color=\"comment\">这是备注（灰色）</font>").append("\n");
+        markdown.append(">").append(new Date());
+        GroupBot.sendMarkdownMsg(GROUP_BOT_KEY, markdown.toString());
+    }
+
+    @Test
     public void sendTextMsg() {
-
         GroupBot.sendTextMsg(GROUP_BOT_KEY, "构建知会：" + new Date());
+        GroupBot.sendTextMsg(GROUP_BOT_KEY, "构建知会 For mentioned_mobile：" + new Date(), null, List.of("13167019278"));
+        GroupBot.sendTextMsg(GROUP_BOT_KEY, "构建知会 For mention userid：" + new Date(), List.of("HB266"), null);
+        GroupBot.sendTextMsg(GROUP_BOT_KEY, "构建知会 For mention everyone：" + new Date(), List.of("@all"), null);
+    }
 
-        try {
-            Thread.sleep(500);
-        } catch (InterruptedException e) {
-            Thread.currentThread().interrupt();
-        }
+    @Test
+    public void testSendImageMsg() throws IOException {
+        GroupBot.sendImageMsg(GROUP_BOT_KEY, IOUtils.toByteArray(GroupBotTest.class.getResourceAsStream("/cat.jpg")));
+    }
 
+    @Test
+    public void testSendNewsMsg() {
+        List<GroupBot.NewsArticle> articles = List.of(new GroupBot.NewsArticle().setTitle("grus-build").setDescription("desc").setUrl("https://www.baidu.com").setPicUrl("http://res.mail.qq.com/node/ww/wwopenmng/images/independent/doc/test_pic_msg1.png"));
+        GroupBot.sendNewsMsg(GROUP_BOT_KEY, articles);
     }
 }
