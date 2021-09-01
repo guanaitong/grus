f38465040008f2e16657f72be81a06dec676e9f2
August <august.zhou@guanaitong.com>
2021-09-01 11:30:56 +0800

    fix sonar rule

--- grus-framework/grus-rabbitmq/src/main/java/com/ciicgat/sdk/mq/MsgDispatcher.java
+++ grus-framework/grus-rabbitmq/src/main/java/com/ciicgat/sdk/mq/MsgDispatcher.java
@@ -20,7 +20,6 @@ import org.slf4j.LoggerFactory;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.util.Objects;
-import java.util.Optional;
 
 
 /**
--- grus-framework/grus-rabbitmq/src/main/java/com/ciicgat/sdk/mq/MsgReceiver.java
+++ grus-framework/grus-rabbitmq/src/main/java/com/ciicgat/sdk/mq/MsgReceiver.java
@@ -20,7 +20,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
-import java.util.Optional;
 import java.util.concurrent.TimeoutException;
 import java.util.concurrent.atomic.AtomicBoolean;
 
