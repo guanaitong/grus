a85aff18cbba8143bff04c961d0b3d94352da663
August <august.zhou@guanaitong.com>
2021-09-15 16:16:35 +0800

    add retry test

--- grus-framework/grus-feign/src/main/java/com/ciicgat/api/core/ConfigRetryer.java
+++ grus-framework/grus-feign/src/main/java/com/ciicgat/api/core/ConfigRetryer.java
@@ -6,6 +6,7 @@
 package com.ciicgat.api.core;
 
 import com.ciicgat.api.core.kubernetes.KubernetesClientConfig;
+import com.ciicgat.sdk.util.frigate.FrigateNotifier;
 import feign.RetryableException;
 import feign.Retryer;
 import org.slf4j.Logger;
@@ -29,6 +30,7 @@ public class ConfigRetryer extends Retryer.Default {
         final KubernetesClientConfig config = KubernetesClientConfig.getConfig();
         if (config.couldRetry(e.getCause())) {
             LOGGER.warn("retry");
+            FrigateNotifier.sendMessageByAppName("feign retry for error: " + e.getMessage());
             super.continueOrPropagate(e);
         } else {
             throw e;
