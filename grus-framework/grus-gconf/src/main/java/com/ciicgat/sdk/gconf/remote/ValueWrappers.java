f10cfb83ae6d013cfcb5d6ddf66eb0aa37afa028
August <august.zhou@guanaitong.com>
2021-09-18 14:46:13 +0800

    use native method

--- grus-framework/grus-gconf/src/main/java/com/ciicgat/sdk/gconf/remote/ValueWrappers.java
+++ grus-framework/grus-gconf/src/main/java/com/ciicgat/sdk/gconf/remote/ValueWrappers.java
@@ -6,17 +6,13 @@
 package com.ciicgat.sdk.gconf.remote;
 
 import com.ciicgat.grus.json.JSON;
-import com.ciicgat.sdk.lang.tool.PropertiesUtils;
 import com.fasterxml.jackson.core.type.TypeReference;
-import org.apache.commons.io.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.IOException;
 import java.io.Reader;
 import java.io.StringReader;
 import java.util.LinkedHashMap;
-import java.util.List;
 import java.util.Properties;
 
 /**
@@ -52,7 +48,7 @@ class ValueWrappers {
                 // NOSONAR
             }
             if (p.size() > 0) {
-                if (!key.endsWith(PropertiesUtils.SUFFIX)) {  //提醒下那些文件名命名不规范的用户
+                if (!key.endsWith(".properties")) {  //提醒下那些文件名命名不规范的用户
                     LOGGER.warn("you file is properties format,but name is" + key, "please change it with .properties suffix");
                 }
                 return new PropertiesValueWrapper(value, p);
@@ -63,15 +59,12 @@ class ValueWrappers {
 
     // Properties.load方法，会把所有的字符串转化为Properties对象，基本不会报错。我们根据properties规则判断下。
     private static boolean isProperties(String content) {
-        try (Reader reader = new StringReader(content)) {
-            List<String> lines = IOUtils.readLines(reader);
-            for (String line : lines) {
-                if (line != null && !line.startsWith("#") && line.contains("=")) {
-                    return true;
-                }
+        return content.lines().filter(line -> {
+            if (line != null && !line.startsWith("#") && !line.startsWith("!") && line.contains("=")) {
+                String[] split = line.split("=", 2);
+                return split.length > 0;
             }
-        } catch (IOException e) {
-        }
-        return false;
+            return false;
+        }).findAny().isPresent();
     }
 }
