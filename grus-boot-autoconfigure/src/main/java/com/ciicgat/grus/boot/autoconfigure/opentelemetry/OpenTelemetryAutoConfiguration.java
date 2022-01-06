f041b0fa079ca8fa56b3ceb2f59a4068290a90d1
August <august.zhou@guanaitong.com>
2022-01-06 16:54:35 +0800

    fix OpenTelemetry npe error

--- grus-boot-autoconfigure/src/main/java/com/ciicgat/grus/boot/autoconfigure/opentelemetry/OpenTelemetryAutoConfiguration.java
+++ grus-boot-autoconfigure/src/main/java/com/ciicgat/grus/boot/autoconfigure/opentelemetry/OpenTelemetryAutoConfiguration.java
@@ -5,7 +5,6 @@
 
 package com.ciicgat.grus.boot.autoconfigure.opentelemetry;
 
-import com.ciicgat.grus.boot.autoconfigure.condition.ConditionalOnServerEnv;
 import io.opentelemetry.api.GlobalOpenTelemetry;
 import io.opentelemetry.api.OpenTelemetry;
 import io.opentelemetry.api.trace.propagation.W3CTraceContextPropagator;
@@ -17,7 +16,7 @@ import io.opentelemetry.sdk.trace.SdkTracerProvider;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingClass;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -25,25 +24,35 @@ import org.springframework.context.annotation.Configuration;
  * Created by August.Zhou on 2019-03-25 14:55.
  */
 @Configuration(proxyBeanMethods = false)
-@ConditionalOnClass({OpenTelemetrySdk.class, JaegerPropagator.class})
-@ConditionalOnProperty(prefix = "grus.opentelemetry", value = "enable", havingValue = "true", matchIfMissing = true)
-@ConditionalOnServerEnv
+
 public class OpenTelemetryAutoConfiguration {
     private static final Logger LOGGER = LoggerFactory.getLogger(OpenTelemetryAutoConfiguration.class);
 
-    @Bean(destroyMethod = "close")
-    public SdkTracerProvider sdkTracerProvider() {
-//        SdkTracerProvider sdkTracerProvider = SdkTracerProvider.builder().addSpanProcessor(SimpleSpanProcessor.create(new LoggingSpanExporter())).build();
-        SdkTracerProvider sdkTracerProvider = SdkTracerProvider.builder().build();
-        return sdkTracerProvider;
+    @ConditionalOnMissingClass("io.opentelemetry.extension.trace.propagation.JaegerPropagator")
+    public static class NoopConfiguration {
+        @Bean
+        public OpenTelemetry openTelemetry() {
+            GlobalOpenTelemetry.set(OpenTelemetry.noop());
+            return GlobalOpenTelemetry.get();
+        }
     }
 
-    @Bean
-    public OpenTelemetry openTelemetry(SdkTracerProvider sdkTracerProvider) {
-        TextMapPropagator textMapPropagator = TextMapPropagator.composite(W3CTraceContextPropagator.getInstance(), JaegerPropagator.getInstance());
-        OpenTelemetrySdk sdk = OpenTelemetrySdk.builder().setTracerProvider(sdkTracerProvider).setPropagators(ContextPropagators.create(textMapPropagator)).build();
-        GlobalOpenTelemetry.set(sdk);
-        return sdk;
+    @ConditionalOnClass({JaegerPropagator.class})
+    public static class SdkConfiguration {
+        @Bean(destroyMethod = "close")
+        public SdkTracerProvider sdkTracerProvider() {
+//        SdkTracerProvider sdkTracerProvider = SdkTracerProvider.builder().addSpanProcessor(SimpleSpanProcessor.create(new LoggingSpanExporter())).build();
+            SdkTracerProvider sdkTracerProvider = SdkTracerProvider.builder().build();
+            return sdkTracerProvider;
+        }
+
+        @Bean
+        public OpenTelemetry openTelemetry(SdkTracerProvider sdkTracerProvider) {
+            TextMapPropagator textMapPropagator = TextMapPropagator.composite(W3CTraceContextPropagator.getInstance(), JaegerPropagator.getInstance());
+            OpenTelemetrySdk sdk = OpenTelemetrySdk.builder().setTracerProvider(sdkTracerProvider).setPropagators(ContextPropagators.create(textMapPropagator)).build();
+            GlobalOpenTelemetry.set(sdk);
+            return sdk;
+        }
     }
 
 
