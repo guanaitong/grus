d993580c3d5d9da3daa3b8eb1638b2796b0fa877
August <august.zhou@guanaitong.com>
2022-01-07 11:26:06 +0800

    fix error

--- grus-framework/grus-core/src/main/java/com/ciicgat/grus/opentelemetry/OpenTelemetrys.java
+++ grus-framework/grus-core/src/main/java/com/ciicgat/grus/opentelemetry/OpenTelemetrys.java
@@ -19,22 +19,22 @@ import java.util.concurrent.atomic.AtomicBoolean;
  * Created by August.Zhou on 2021/12/17 17:53.
  */
 public class OpenTelemetrys {
-    private static final AtomicBoolean SET_STATE = new AtomicBoolean(false);
+    private static final AtomicBoolean STATE = new AtomicBoolean(true);
 
     public static void set(OpenTelemetry openTelemetry) {
-        SET_STATE.set(true);
+        STATE.set(false);
         GlobalOpenTelemetry.set(openTelemetry);
     }
 
     public static TextMapPropagator getTextMapPropagator() {
-        if (!SET_STATE.get()) {
+        if (STATE.get()) {
             return TextMapPropagator.noop();
         }
         return GlobalOpenTelemetry.get().getPropagators().getTextMapPropagator();
     }
 
     public static Tracer getTracer() {
-        if (!SET_STATE.get()) {
+        if (STATE.get()) {
             return TracerProvider.noop().get("grus-instrumentation", "1.0.0");
         }
         return GlobalOpenTelemetry.get().getTracer("grus-instrumentation", "1.0.0");
